08152f5d5fb58931c1c8758b420cf8f9655c01a3
Cathy <cathy.lin.0112@gmail.com>
2020-11-21 11:23:27 +0800

    Timer

--- a311/ViewController.swift
+++ a311/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    var counter = 0
+    var timer:Timer!
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(showCounter), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func showCounter(){
+        print(counter)
+        counter += 1
+    }
+        
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisapper")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
     }
 
 
