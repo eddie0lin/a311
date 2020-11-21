f1f2a8f6891aa722dc482f2b44d29b4aa85e69af
Cathy <cathy.lin.0112@gmail.com>
2020-11-21 17:28:38 +0800

    VCLife

--- a311/ViewController.swift
+++ a311/ViewController.swift
@@ -9,26 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var counter = 0
+    var counter:Double = 0
     var timer:Timer!
+    var offset:Double = 1.0
+    
+    @IBOutlet weak var dir: UISegmentedControl!
+    @IBOutlet weak var speed: UISlider!
     
     override func viewDidLoad(){
         super.viewDidLoad()
         print("viewDidLoad")
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(showCounter), userInfo: nil, repeats: true)
     }
-    
-    
-    @objc func showCounter(){
-        print(counter)
-        counter += 1
-    }
-        
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("viewWillAppear")
+        
+        let newView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+          newView.center = self.view.center
+          newView.backgroundColor = UIColor.purple
+          newView.transform = CGAffineTransform(rotationAngle: 0)
+          newView.layer.cornerRadius = 15
+          newView.clipsToBounds = true
+          view.addSubview(newView)
+         
+          timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (timer) in
+              self.rotateView(targateView: newView)
+          })
     }
+    
+    func rotateView(targateView:UIView){
+        let angle =  counter * .pi / 180
+        targateView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        targateView.center = self.view.center
+        print(speed.value)
+        counter += offset * Double(speed.value)
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("viewDidAppear")
@@ -36,12 +53,34 @@ class ViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         print("viewWillDisapper")
+        timer.invalidate()
+        for view in self.view.subviews{
+                    if let button = view as? UIButton{
+                        print(button)
+                    }else{
+                        view.removeFromSuperview()
+                    }
+                }
     }
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         print("viewDidDisappear")
     }
+    @IBAction func setdirect(_ sender: UISegmentedControl) {
+        print(sender.selectedSegmentIndex)
+        switch sender.selectedSegmentIndex {
+        case 0:
+            offset = 1
+        case 1:
+            offset = -1
+        default:
+            break
+            
+        }
+    }
+    
 
-
+    
+    
 }
 
